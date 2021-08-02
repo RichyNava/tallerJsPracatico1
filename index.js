7b34f7f0fc4c2659096f24cb5f2891c121611301
RichyNava <richynava2000@outlook.com>
2021-08-02 16:22:48 -0500

    actualizando los archivos index

---
+++ index.js
@@ -0,0 +1,11 @@
+function butPerCuadrado(){
+    var input=document.getElementById('inputCuadrado');
+    var value= input.value;
+    return alert(value*4);
+}
+
+function butAreaCuadrado(){
+    var area=document.getElementById('inputCuadrado');
+    var value=area.value;
+    return alert(value*value);
+}
